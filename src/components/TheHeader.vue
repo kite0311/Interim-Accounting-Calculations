a828a4a254ee989f2f0cd752737fff3f37e92d91
Kaito Kannari <19970311kk@gmail.com>
2023-10-03 02:26:36 +0900

    不要箇所の削除

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,26 +1,8 @@
 <template>
   <h1 class="title">Calculate</h1>
-  <!--TODO 下記は後で削除するかもしれない-->
-  <!-- <nav>
-    <h3>メインメニュー(仮)</h3>
-    <ul class="header">
-      <li><a href="/">メイン１</a></li>
-      <li><a href="/">メイン２</a></li>
-      <li><a href="/">メイン３</a></li>
-      <li><a href="/">メイン４</a></li>
-    </ul>
-  </nav> -->
 </template>
 
 <style scoped>
-/* .header {
-  display: flex;
-  list-style: none;
-}
-
-ul li {
-  margin-left: 2em;
-} */
 .title {
   width: 100%;
   background-color: #e3f2fd;
