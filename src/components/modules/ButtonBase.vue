b1c14376d93202b5b0594e73e484d6994aa25bc7
Kaito Kannari <19970311kk@gmail.com>
2023-10-07 02:13:00 +0900

    ボタンタグのベースを追加

---
+++ src/components/modules/ButtonBase.vue
@@ -0,0 +1,35 @@
+<script setup>
+const props = defineProps({ color: String });
+</script>
+
+<template>
+  <button class="btn" :class="props.color"><slot /></button>
+</template>
+
+<style scoped>
+.btn:active {
+  box-shadow: none;
+  transform: translateY(5px);
+}
+.btn {
+  text-transform: uppercase;
+  text-decoration: dotted;
+  margin: 30px;
+  color: white;
+  width: 130px;
+  height: 42px;
+  border-radius: 3px;
+  border: none;
+  display: block;
+}
+
+.orange {
+  background-color: #ffa500;
+  box-shadow: 0 5px 0 #ec9a00;
+}
+
+.blue {
+  background-color: #5392f0;
+  box-shadow: 0 5px 0 #3880ecd8;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-import './assets/btn/style.css';
 import './assets/card/style.css';
 
 createApp(App).mount('#app');
