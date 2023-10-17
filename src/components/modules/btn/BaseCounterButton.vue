5f0d825df510e84aef459ab15be0c360278d049b
Kaito Kannari <19970311kk@gmail.com>
2023-10-18 01:49:30 +0900

    カウンターボタンのBase

---
+++ src/components/modules/btn/BaseCounterButton.vue
@@ -0,0 +1,27 @@
+<script setup>
+const props = defineProps({ counter: String });
+const emit = defineEmits(['on-click']);
+
+const onClick = (str) => {
+  emit('on-click', str);
+};
+</script>
+
+<template>
+  <button class="btn" :class="props.counter" @click="onClick('click')"><slot /></button>
+</template>
+
+<style scoped>
+.btn:active {
+  box-shadow: none;
+  transform: translateY(2px);
+}
+.btn {
+  margin: 2px;
+  color: white;
+  width: 36px;
+  height: 36px;
+  border-radius: 18px;
+  border: none;
+}
+</style>
